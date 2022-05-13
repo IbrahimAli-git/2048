feb9082aceb062b1d62ab9743018d61aaea3aaa2
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-13 09:14:43 +0100

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -18,20 +18,20 @@ public class Game2048 extends Game {
     }
 
 
-    private void createGame(){
+    private void createGame() {
         createNewNumber();
         createNewNumber();
     }
 
-    private void drawScene(){
+    private void drawScene() {
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
-                setCellColor(i, j, Color.FIREBRICK);
+                setCellColoredNumber(i, j, gameField[j][i]);
             }
         }
     }
 
-    private void createNewNumber(){
+    private void createNewNumber() {
         int x, y, z;
         do {
             x = getRandomNumber(SIDE);
@@ -43,5 +43,31 @@ public class Game2048 extends Game {
         if (z == 9) gameField[x][y] = 4;
         else gameField[x][y] = 2;
     }
-}
 
+    private Color getColorByValue(int value) {
+        switch (value){
+            case 0 : return Color.BLACK;
+            case 2 : return Color.RED;
+            case 4 : return Color.BLUE;
+            case 8 : return Color.CHOCOLATE;
+            case 16 : return Color.FORESTGREEN;
+            case 32 : return Color.DARKSLATEGRAY;
+            case 64 : return Color.HONEYDEW;
+            case 128 : return Color.LIMEGREEN;
+            case 256 : return Color.PURPLE;
+            case 512 : return Color.VIOLET;
+            case 1024 : return Color.SNOW;
+            case 2048 : return Color.WHEAT;
+            default: return Color.NONE;
+        }
+    }
+
+    private void setCellColoredNumber(int x, int y, int value) {
+       Color color =  getColorByValue(value);
+       if (value == 0){
+           setCellValueEx(x, y, color, "");
+       } else {
+           setCellValueEx(x, y, color, String.valueOf(value));
+       }
+    }
+}
