8d0a54635bfeb28449cc5bb864e4e5617761a802
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-13 08:12:12 +0100

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -3,5 +3,15 @@ package com.codegym.games.game2048;
 import com.codegym.engine.cell.*;
 
 public class Game2048 extends Game {
+    private static final int SIDE = 4;
 
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
+
