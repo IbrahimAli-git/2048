9c9404bdf806fd3f154f8c42c497a0dd747f021c
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-16 11:30:16 +0100

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -4,84 +4,82 @@ import com.codegym.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
-    private int[][] gameField = new int[SIDE][SIDE];
+    private int gameField[][] = new int[SIDE][SIDE];
+
+    public static void main(String[] args) {
+
+    }
 
     @Override
     public void initialize() {
-        setScreenSize(SIDE, SIDE);
         createGame();
+        setScreenSize(SIDE, SIDE);
         drawScene();
     }
 
-    @Override
-    public void onKeyPress(Key key) {
-        if (key == Key.UP) {
-            moveUp();
-        } else if (key == Key.RIGHT) {
-            moveRight();
-        } else if (key == Key.DOWN) {
-            moveDown();
-        } else if (key == Key.LEFT) {
-            moveLeft();
-        } else {
-            return;
-        }
-        drawScene();
-    }
 
     private void createGame() {
         createNewNumber();
         createNewNumber();
     }
 
-    private void createNewNumber() {
-        boolean isCreated = false;
-        do {
-            int x = getRandomNumber(SIDE);
-            int y = getRandomNumber(SIDE);
-            if (gameField[y][x] == 0) {
-                gameField[y][x] = getRandomNumber(10) < 9 ? 2 : 4;
-                isCreated = true;
+    private void drawScene() {
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                setCellColoredNumber(i, j, gameField[j][i]);
             }
         }
-        while (!isCreated);
     }
 
-    private void setCellColoredNumber(int x, int y, int value) {
-        Color color = getColorByValue(value);
-        String str = value > 0 ? "" + value : "";
-        setCellValueEx(x, y, color, str);
-    }
-
-    private Color getColorByValue(int value) {
-        switch (value) {
-            case 0:
-                return Color.WHITE;
-            case 2:
-                return Color.PLUM;
-            case 4:
-                return Color.SLATEBLUE;
-            case 8:
-                return Color.DODGERBLUE;
-            case 16:
-                return Color.DARKTURQUOISE;
-            case 32:
-                return Color.MEDIUMSEAGREEN;
-            case 64:
-                return Color.LIMEGREEN;
-            case 128:
-                return Color.DARKORANGE;
-            case 256:
-                return Color.SALMON;
-            case 512:
-                return Color.ORANGERED;
-            case 1024:
-                return Color.DEEPPINK;
-            case 2048:
-                return Color.MEDIUMVIOLETRED;
-            default:
-                return Color.NONE;
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key){
+            case UP: moveUp(); break;
+            case RIGHT: moveRight(); break;
+            case DOWN: moveDown(); break;
+            case LEFT: moveLeft(); break;
         }
+        if (key == Key.DOWN || key == Key.UP || key == Key.LEFT || key == Key.RIGHT){
+            drawScene();
+        }
+    }
+
+    private void rotateClockwise() {
+        for (int i = 0; i < SIDE / 2; i++) {
+            for (int j = i; j < SIDE - i - 1; j++) {
+                // Swap elements of each cycle in clockwise direction
+                int temp = gameField[i][j];
+                gameField[i][j] = gameField[SIDE - 1 - j][i];
+                gameField[SIDE - 1 - j][i] = gameField[SIDE - 1 - i][SIDE - 1 - j];
+                gameField[SIDE - 1 - i][SIDE - 1 - j] = gameField[j][SIDE - 1 - i];
+                gameField[j][SIDE - 1 - i] = temp;
+            }
+        }
+    }
+
+
+    private void moveDown() {
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
+    }
+
+    private void moveRight() {
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
+    }
+
+    private void moveUp() {
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
     }
 
     private void moveLeft() {
@@ -101,51 +99,78 @@ public class Game2048 extends Game {
         }
     }
 
-    private void moveRight() {
 
+    private void createNewNumber() {
+        int x, y, z;
+        do {
+            x = getRandomNumber(SIDE);
+            y = getRandomNumber(SIDE);
+        } while (gameField[x][y] != 0);
+
+        z = getRandomNumber(10);
+
+        if (z == 9) gameField[x][y] = 4;
+        else gameField[x][y] = 2;
     }
 
-    private void moveUp() {
-
-    }
-
-    private void moveDown() {
-
-    }
-
-    private boolean compressRow(int[] row) {
-        int insertPosition = 0;
-        boolean result = false;
-        for (int x = 0; x < SIDE; x++) {
-            if (row[x] > 0) {
-                if (x != insertPosition) {
-                    row[insertPosition] = row[x];
-                    row[x] = 0;
-                    result = true;
-                }
-                insertPosition++;
-            }
+    private Color getColorByValue(int value) {
+        switch (value){
+            case 0 : return Color.BLACK;
+            case 2 : return Color.RED;
+            case 4 : return Color.BLUE;
+            case 8 : return Color.CHOCOLATE;
+            case 16 : return Color.FORESTGREEN;
+            case 32 : return Color.DARKSLATEGRAY;
+            case 64 : return Color.HONEYDEW;
+            case 128 : return Color.LIMEGREEN;
+            case 256 : return Color.PURPLE;
+            case 512 : return Color.VIOLET;
+            case 1024 : return Color.SNOW;
+            case 2048 : return Color.WHEAT;
+            default: return Color.NONE;
         }
-        return result;
     }
 
-    private boolean mergeRow(int[] row) {
-        boolean result = false;
+    private void setCellColoredNumber(int x, int y, int value) {
+       Color color =  getColorByValue(value);
+       if (value == 0){
+           setCellValueEx(x, y, color, "");
+       } else {
+           setCellValueEx(x, y, color, String.valueOf(value));
+       }
+    }
+
+    private boolean compressRow(int[] row){
+        int[] tempArray = row.clone();
+        boolean isChanged = false;
+
         for (int i = 0; i < row.length - 1; i++) {
-            if (row[i] != 0 && row[i] == row[i + 1]) {
-                row[i] += row[i + 1];
-                row[i + 1] = 0;
-                result = true;
+            for (int j = 0; j < row.length - 1 - i; j++) {
+                int current = row[j];
+                int temp = row[j + 1];
+                if (current == 0) {
+                    row[j] = temp;
+                    row[j + 1] = current;
+                    isChanged = true;
+                }
             }
         }
-        return result;
+
+        return isChanged;
     }
 
-    private void drawScene() {
-        for (int y = 0; y < SIDE; y++) {
-            for (int x = 0; x < SIDE; x++) {
-                setCellColoredNumber(x, y, gameField[y][x]);
+    private boolean mergeRow(int[] row){
+        int[] tempArray = row.clone();
+        boolean isChanged = false;
+
+        for (int i = 0; i < row.length - 1; i++) {
+            if (row[i] == row[i+1]){
+                row[i] += row[i+1];
+                row[i+1] = 0;
+                isChanged = true;
             }
         }
+
+        return isChanged;
     }
 }
